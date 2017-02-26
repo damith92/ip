f30fe810c1231fd8cdf22cbfbee976579e95a60d
udarajag <udarajag@gmail.com>
2017-02-27 00:30:29 +0800

    Initial commit

---
+++ ip_lec2_test/src/main/java/com/ip/dao/AbstractDao.java
@@ -0,0 +1,17 @@
+package com.ip.dao;
+
+import org.hibernate.SessionFactory;
+
+public class AbstractDao {
+	
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+}
